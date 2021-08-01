68c424af3d2ca02c4f54c07ff57d28bcf00e2185
SeokhaKang <SeokhaKangCS@gmail.com>
2021-08-01 16:45:15 +0900

    Second commit 2

--- app/src/main/java/com/seokha/exercise_twice/ImageInsideActivity.kt
+++ app/src/main/java/com/seokha/exercise_twice/ImageInsideActivity.kt
@@ -2,10 +2,25 @@ package com.seokha.exercise_twice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 
 class ImageInsideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_inside)
+
+        var index:Int = intent.getIntExtra("index", 0)
+        Log.e("index", index.toString())
+
+        var resID = resources.getIdentifier(
+            "member_$index",
+            "drawable",
+            packageName
+        )
+        var image = findViewById<ImageView>(R.id.inside_activity)
+        image.setImageResource(resID)
+        Log.e("Check-2", resID.toString())
     }
 }
+
--- app/src/main/java/com/seokha/exercise_twice/MainActivity.kt
+++ app/src/main/java/com/seokha/exercise_twice/MainActivity.kt
@@ -15,33 +15,34 @@ class MainActivity : AppCompatActivity() {
 //        {
 //            var IDs : Int = resources.getIdentifier("member_"+(i+1), "drawable", "com.seokha.exercise_twice")
 //        }
-        var btn1 = findViewById<ImageView>(R.id.member_1)
-        Log.e("Check1", R.id.member_1.toString())
-        btn1.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            startActivity(intent)
-        }
+//        var btn1 = findViewById<ImageView>(R.id.member_1)
+//        Log.e("Check1", R.id.member_1.toString())
+//        btn1.setOnClickListener{
+//            val intent = Intent(this, ImageInsideActivity::class.java)
+//            startActivity(intent)
+//        }
         var resIDs = Array<Int>(9, {0})
         var buttons = arrayOfNulls<ImageView>(9)
 
-        for(i in 1..9) {
+        for(i in 0..8) {
             var resID = resources.getIdentifier(
-                "member_$i",
+                "member_" + (i+1),
                 "id",
                 packageName
             )
-            var picID = resources.getIdentifier(
-                "member_$i",
-                "drawable",
-                packageName
-            )
-            resIDs[i-1] = resID
-            buttons[i-1] = findViewById<ImageView>(resIDs[i-1])
-            buttons[i-1]?.setOnClickListener{
+//            var picID = resources.getIdentifier(
+//                "member_$i",
+//                "drawable",
+//                packageName
+//            )
+            resIDs[i] = resID
+            buttons[i] = findViewById<ImageView>(resIDs[i])
+            buttons[i]?.setOnClickListener{
                 val intent = Intent(this, ImageInsideActivity::class.java)
+                intent.putExtra("index", i+1)
                 startActivity(intent)
             }
-            Log.e("Check", resIDs[i-1].toString())
+            Log.e("Check", resIDs[i].toString())
 
         }
     }
